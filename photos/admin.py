d007f7b977d8adc0e7de01cf0280882949ce9d2d
Irene-nandy <irenekasiva75@gmail.com>
2020-10-12 02:35:38 +0300

    admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,8 +1,8 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# # Register your models here.
-# from .models import Category,Image,Location
+# Register your models here.
+from .models import Category,Image,Location
 
-# admin.site.register(Category),
-# admin.site.register(Image),
-# admin.site.register(Location)
+admin.site.register(Category),
+admin.site.register(Image),
+admin.site.register(Location)
